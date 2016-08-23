dc684f3b1f04e2fac44ebe106409bd50aaf20e21
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2016-08-23 17:13:58 -0400

    made all paths smart urls

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,11 @@ Rails.application.routes.draw do
   root 'pages#index'
 
   get '/home' => 'pages#home'
-
   get '/profile' => 'pages#profile'
-
   get '/explore' => 'pages#explore'
-
   get '/help' => 'pages#help'
-
   get '/edit' => 'pages#edit'
-
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
