9e84702946ae38cbf17237e01ed28e754f9850df
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2016-08-23 16:40:07 -0400

    generated Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+
+  get 'pages/home'
+
+  get 'pages/profile'
+
+  get 'pages/explore'
+
+  get 'pages/help'
+
+  get 'pages/edit'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
