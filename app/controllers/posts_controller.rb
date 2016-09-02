5b6de47b915532c28b0a0f875dda874781965d15
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2016-09-02 04:27:26 -0400

    wip; wrote conditional saved/not saved logic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,19 @@
 class PostsController < ApplicationController
   def new
-    @newPost = Post.new
+    @post = Post.new
   end
 
   def create
-    
+    @post = Post.new
+    @post.user_id = current_user.id
+    respond_to do |f|
+      if (@post.save)
+        f.html { redirect_to '', notice: 'Post created!' }
+      else
+        f.html { redirect_to '', notice: 'Error: Post not saved!' }
+      end
+    end
+
   end
+
 end
