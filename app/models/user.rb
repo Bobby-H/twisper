2de2c2fa944eb9b446067e09556f3c0a080a4479
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2016-12-14 11:36:49 -0500

    follower relationship & btn implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
          has_many :posts, dependent: :destroy
-         has_many :active_relationships, class_name: "relationship", foreign_key: "follower_id", dependent: :destroy
-         has_many :passive_relationships, class_name: "relationship", foreign_key: "followed_id", dependent: :destroy
+         has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+         has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 
          has_many :following, through: :active_relationships, source: :followed
-         has_many :following, through: :passive_relationships, source: :follower
+         has_many :followers, through: :passive_relationships, source: :follower
 
          #helper methods
 
